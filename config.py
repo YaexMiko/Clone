b25cc44add11acff699290a0f3472efaf897a410
rohit3607 <rohitplayer87089@gmail.com>
2024-12-01 22:37:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,8 +33,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "FilesStoringBot")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002485124074"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002201572719"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002215102799"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002485124074"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
